6040a355ca694d76fcfcb253a4fd8d3a2fcd8d62
Gili Tzabari <cowwoc@bbs.darktech.org>
2009-10-09 23:43:06 +0000

    Added test program

---
+++ java/source/jperipheral/Main.java
@@ -0,0 +1,84 @@
+package jperipheral;
+
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * @author Gili
+ */
+public class Main
+{
+	private AsynchronousByteCharChannel channel;
+	private final String portName = "COM5";
+
+	public static void main(String[] args) throws Exception
+	{
+		Main main = new Main();
+		main.run();
+	}
+
+	private int unsigned(int signed)
+	{
+		return signed + 128;
+	}
+
+	private void run() throws Exception
+	{
+		flush();
+		send("version");
+		send("get 0");
+		send("get state", "select 1");
+		send("select 1");
+		send("select a", "get state");
+	}
+
+	private void flush() throws Exception
+	{
+		SerialPort port = SerialPort.getByName(portName);
+		port.configure(9600, SerialPort.DataBits.EIGHT, SerialPort.Parity.NONE, SerialPort.StopBits.ONE,
+			SerialPort.FlowControl.NONE);
+		channel = AsynchronousByteCharChannel.open(port.getAsynchronousChannel(),
+			Charset.forName("UTF-8"));
+		System.out.println("Flushing...");
+		try
+		{
+			while (true)
+				System.out.println("got : " + channel.readLine().get(1, TimeUnit.SECONDS));
+		}
+		catch (TimeoutException e)
+		{
+			// eventually there is no more data to read...
+		}
+		finally
+		{
+			//System.out.println("Closing\n\n");
+			port.close();
+		}
+	}
+
+	private void send(String... lines) throws Exception
+	{
+		SerialPort port = SerialPort.getByName(portName);
+		port.configure(9600, SerialPort.DataBits.EIGHT, SerialPort.Parity.NONE, SerialPort.StopBits.ONE,
+			SerialPort.FlowControl.NONE);
+		channel = AsynchronousByteCharChannel.open(port.getAsynchronousChannel(),
+			Charset.forName("UTF-8"));
+		try
+		{
+			for (String line: lines)
+			{
+				System.out.println("sending: " + line);
+				channel.write(CharBuffer.wrap(line + "\r"), false).get(1, TimeUnit.SECONDS);
+				System.out.println("got : " + channel.readLine().get(1000, TimeUnit.SECONDS));
+			}
+		}
+		finally
+		{
+//			System.out.println("Closing\n\n");
+			port.close();
+		}
+	}
+}

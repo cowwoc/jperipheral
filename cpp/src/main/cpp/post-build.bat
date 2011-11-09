956f6e4133d002750af6ac4aec0565c9a95c0c4b
Gili Tzabari <cowwoc@bbs.darktech.org>
2011-11-08 21:07:06 -0500

    Debug build was including release libraries, and vice versa; fixed.

--- cpp/src/main/cpp/post-build.bat
+++ cpp/src/main/cpp/post-build.bat
@@ -1,12 +1,9 @@
 @echo off
 rem *******************************
 rem * %1 = build configuration    *
-rem * %2 = jace library directory *
+rem * %2 = dependency directory   *
 rem * %3 = output directory       *
 rem *******************************
 
 echo Copying dependencies to output directory...
-copy "%2\*.dll" "%3\%1" /y
-if /i %1 == "debug" (
-	copy "%2\*.pdb" "%3\%1" /y
-)
+copy "%2\*" "%3\%1" /y
